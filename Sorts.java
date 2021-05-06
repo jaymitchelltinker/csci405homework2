8a34b0c10f90fd60412bb8cb4172e2220de9dab6
CullenOwen <40608151+CullenOwen@users.noreply.github.com>
2021-05-05 20:42:28 -0700

    Sorts.java skeleton code is implemented for createArray, reverseArray, unsortArray

--- Sorts.java
+++ Sorts.java
@@ -1,21 +1,28 @@
 import java.util.Random;
-import java.util.Math;
+// import java.util.Math;
 
-import ShellSort;
-import ShannonSort;
-import InsersionSort;
+// import ShellSort;
+// import ShannonSort;
+// import InsersionSort;
 
 public class Sorts {
-
+    
     /*  unsortArray
-     *      takes an array that is already sorted and unsorts a percentage of it given as a parameter
-     */
+    *      takes an array that is already sorted and unsorts a percentage of it given as a parameter
+    */
     public static void unsortArray(int[] arr, int percentSort) {
         if (percentSort > 100 || percentSort < 0)
         {
             System.out.println("percentSort is incorrect");
             return;
         }
+        Random rand = new Random();
+        int sortVal = 100/percentSort;
+
+        for (int i = 0; i < (arr.length/sortVal); i++) {
+            arr[i] = rand.ints(arr.length * -2, (arr.length/sortVal)).limit(1).findFirst().getAsInt();
+        }
+
     }
 
     /*  createArray
@@ -24,6 +31,10 @@ public class Sorts {
     public static int[] createArray(int sz) {
         int[] arr = new int[sz];
 
+        for (int i = 0; i < sz; i++) {
+            arr[i] = i;
+        }
+
         return arr;
     }
 
@@ -31,14 +42,36 @@ public class Sorts {
      *      takes a given array and reverses the order of a percentage of the array
      */
     public static void reverseArray(int[] arr, int percentSort) {
-        if (percentSort > 100 || percentSort < 0)
+
+        if (percentSort > 100 || percentSort <= 0)
         {
-            System.out.println("percentSort is incorrect");
+            System.out.println("percentSort must be larger than 0 and less than 100.");
             return;
+        }
+
+        int sortVal = 100/percentSort;
+
+        System.out.println("SORTVAL:: " + sortVal);
+        System.out.println("i LIMIT:: " + arr.length/sortVal);
+
+        for (int i = 0; i < (arr.length/sortVal)/2; i++) {
+            System.out.println("Swapping " + arr[i] + ", " + arr[((arr.length/sortVal) - 1) - i]);
+            int temp = arr[i];
+            arr[i] = arr[((arr.length/sortVal) - 1) - i];
+            arr[((arr.length/sortVal) - 1) - i] = temp;
         }
     }
 
     public static void main(String args[]) {
+        int[] myArr = createArray(15);
 
+        // reverseArray(myArr, 20);
+        unsortArray(myArr, 50);
+
+        for (int i = 0; i < myArr.length; i++) {
+            System.out.print(myArr[i] + " ");
+        }
+
+        System.out.println();
     }
 }
